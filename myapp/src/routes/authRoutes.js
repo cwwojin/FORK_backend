574e055f25149b904d6a759c33a983697cbfb04e
cwwojin <cwwojin@gmail.com>
2024-05-09 03:03:26 +0900

    fix : fix auth router, add missing dependencies
    - router definition added to "authRoutes.js" which is used as middleware in "app.js"
    - code consistencies
    - missing dependencies for bcrypt, jsonwebtoken added

--- myapp/src/routes/authRoutes.js
+++ myapp/src/routes/authRoutes.js
@@ -4,4 +4,16 @@ const authController = require("../controllers/authController");
 const { body, param, query } = require("express-validator");
 const { validatorChecker } = require("../middleware/validator");
 
+router
+    .post(
+        '/login',
+        [
+            body('userId').exists().notEmpty().isLength({max: 20}),
+            body('password').exists().notEmpty().isLength({min: 6, max: 20}),
+            validatorChecker,
+        ],
+        authController.loginUser
+    )
+;
+
 module.exports = router;
