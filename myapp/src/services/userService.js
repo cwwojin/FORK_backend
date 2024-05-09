a7f1b54f111b69b10b07ddf3f48e2722084f4f51
cwwojin <cwwojin@gmail.com>
2024-05-09 15:00:44 +0900

    feat : add user preference & favorite
    - get, add, delete user preferences
    - get, add, delete user favorites
    - edit package.json scripts

--- myapp/src/services/userService.js
+++ myapp/src/services/userService.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const db = require("../models/index");
 
 module.exports = {
@@ -9,12 +10,12 @@ module.exports = {
         const result = await db.query(query);
         return result.rows;
     },
-    // get user by accountID
+    // get user by id
     getUserById: async (id) => {
         const query = {
-            text: 'select * from "user" where account_id = $1',
+            text: 'select * from "user" where id = $1',
             values: [id],
-        }
+        };
         const result = await db.query(query);
         return result.rows;
     },
@@ -29,9 +30,92 @@ module.exports = {
                 info.email,
                 info.displayName,
             ]
+        };
+        const result = await db.query(query);
+        return result.rows;
+    },
+    // update user - profile (password, email, display_name)
+    updateUserProfile: async (info, id) => {
+        const query = {
+            text: 'update "user" set password = $1, email = $2, display_name = $3 where id = $4 returning *',
+            values: [
+                info.password,
+                info.email,
+                info.displayName,
+                id,
+            ]
+        };
+        const result = await db.query(query);
+        return result.rows;
+    },
+    deleteUser: async (id) => {
+        const query = {
+            text: 'delete from "user" where id = $1',
+            values: [id],
+        };
+        const result = await db.query(query);
+        return result;
+    },
+    // get user preferences
+    getUserPreference: async (id) => {
+        const query = {
+            text: 'select p.* from user_preference up join "user" u on up.user_id = u.id join preference p on up.preference_id = p.id where up.user_id = $1',
+            values: [id],
         }
         const result = await db.query(query);
         return result.rows;
+    },
+    // add a preference to user (if it isn't already added)
+    addUserPreference: async (user_id, pref_id) => {
+        const query = {
+            text: `insert into user_preference (user_id, preference_id) values ($1, $2)
+                on conflict on constraint user_preference_pkey do nothing
+                returning *`,
+            values: [user_id, pref_id],
+        }
+        const result = await db.query(query);
+        return result.rows;
+    },
+    // delete a preference of a user
+    deleteUserPreference: async (user_id, pref_id) => {
+        const query = {
+            text: `delete from user_preference where user_id = $1 and preference_id = $2`,
+            values: [user_id, pref_id],
+        }
+        const result = await db.query(query);
+        return result;
+    },
+    // get user favorites
+    getUserFavorite: async (id) => {
+        const query = {
+            text: `select f.* from favorite fv 
+                join "user" u on fv.user_id = u.id 
+                join facility f on fv.facility_id = f.id
+                where fv.user_id = $1`,
+            values: [id],
+        }
+        const result = await db.query(query);
+        return result.rows;
+    },
+    // add a favorite to user (if it isn't already added)
+    addUserFavorite: async (user_id, facility_id) => {
+        const query = {
+            text: `insert into favorite (user_id, facility_id) values ($1, $2)
+                on conflict on constraint favorite_pkey do nothing
+                returning *`,
+            values: [user_id, facility_id],
+        }
+        const result = await db.query(query);
+        return result.rows;
+    },
+    // delete a favorite of a user
+    deleteUserFavorite: async (user_id, facility_id) => {
+        const query = {
+            text: `delete from favorite where user_id = $1 and facility_id = $2`,
+            values: [user_id, facility_id],
+        }
+        const result = await db.query(query);
+        return result;
     },
 
 }
