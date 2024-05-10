e2fe00b013de9bf33d7389f875ab945d32d74dd7
cwwojin <cwwojin@gmail.com>
2024-05-11 01:34:23 +0900

    feat : check userId and password constraints
    - check constraints for userId and password during param validation
    - both : length min=6, max=20, consists of alphanumeric, ".", "_", "-"

--- myapp/src/helper/helper.js
+++ myapp/src/helper/helper.js
@@ -1,3 +1,10 @@
+const userNamePattern = new RegExp("^[a-zA-Z0-9._\-]+$");
+const passwordPattern = new RegExp("^[a-zA-Z0-9._\-]+$");
+
 module.exports = {
     USER_TYPES: [0,1,2],
+    validateUserId: (userId) => 
+        userNamePattern.test(userId),
+    validatePassword: (password) => 
+        passwordPattern.test(password),
 }
