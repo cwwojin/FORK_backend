e318a8a52fa32f54f30486635e643333e766ef95
cwwojin <cwwojin@gmail.com>
2024-05-23 16:26:52 +0900

    feat : add ESLint dependency & config
    - add ESLint dependency
    - config, plugins : recommended, prettier, import
    - new script for linting

---
+++ myapp/eslint.config.js
@@ -0,0 +1,28 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+const prettier = require("eslint-config-prettier");
+const importPlugin = require("eslint-plugin-import");
+
+module.exports = [
+    js.configs.recommended,
+    prettier,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            }
+        },
+        plugins: { import: importPlugin },
+        rules: {
+            "no-unused-vars": "warn",
+            "prefer-const": "warn",
+            "import/order": [
+                "warn",
+                {
+                  "groups": ["builtin", "external", ["parent", "sibling"], "index"],
+                  "newlines-between": "always"
+                }
+            ],
+        },
+    }
+];
